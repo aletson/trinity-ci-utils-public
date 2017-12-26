a79122c9e9676e08f8f648acbe7ed85173d30532
Andrew Letson <velinath@users.noreply.github.com>
2017-12-26 12:45:03 -0500

    use clang

--- ami-create.sh
+++ ami-create.sh
@@ -2,11 +2,10 @@
 # This file should be run to prepare a VM for having an image taken
 # This should only need to be run once, then use the created image to spin up your build machine.
 # Remember to periodically run apt-get update && apt-get upgrade and recapture the image.
-echo 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main' | sudo tee -a /etc/apt/sources.list
-echo 'deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main' | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install git cmake make clang-5.0 libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev p7zip zip
+sudo apt-get install git cmake make clang libmariadbclient-dev libssl1.0-dev libbz2-dev libreadline-dev libncurses-dev libboost-all-dev p7zip zip unzip build-essential
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang 100
 sudo apt-get install openjdk-8-jre # For Jenkins
 mkdir -p ~/.aws
 cat <<EOF > ~/.aws/credentials
